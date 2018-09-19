cd991727ffc04e9baa8dcbeeffd4471b9bcb7478
Celina Lindqvist <celinalindqvist@Celinas-MacBook-Air.local>
2018-09-19 12:37:47 +0200

    Rettelser

--- javascript.js
+++ javascript.js
@@ -1,102 +1,107 @@
- document.addEventListener("DOMContentLoaded", hentJson);
- let urlParams = new URLSearchParams(window.location.search);
- let id = urlParams.get("id");
+document.addEventListener("DOMContentLoaded", hentJson);
+let urlParams = new URLSearchParams(window.location.search);
+let id = urlParams.get("id");
 
 
- let dest = document.querySelector(".data-container"),
-     retter,
-     kategoriFilter = "alle";
+let dest = document.querySelector(".data-container"),
+    retter,
+    kategoriFilter = "alle";
 
+//Indlæser json fil, lægger retterne i arrayet//
+async function hentJson() {
+    let jsonData = await fetch("json/menu.json");
+    retter = await jsonData.json();
 
- async function hentJson() {
-     let jsonData = await fetch("json/menu.json");
-     retter = await jsonData.json();
+    //console.log(retter);
 
-     //console.log(retter);
+    visRetter();
+}
 
-     visRetter();
- }
+//Ved klik på .menu-item kaldes funktionen filtering
 
- document.querySelectorAll(".menu-item").forEach(knap => {
-     //console.log(knap);
-     knap.addEventListener("click", filtering)
+document.querySelectorAll(".menu-item").forEach(knap => {
+    //console.log(knap);
+    knap.addEventListener("click", filtering)
 
- });
+});
 
- function filtering() {
+// filterer kategorierne så de bliver opdelt alt efter deres kategori i json filen
+function filtering() {
 
-     kategoriFilter = this.getAttribute("data-kategori");
-     //console.log(kategoriFilter);
-     visRetter();
-
-
-
- }
-
- function visRetter() {
-     let temp = document.querySelector(".retter-template");
-     let modtager = document.querySelector(".main-container");
-     modtager.textContent = "";
-     document.querySelector("header h3").textContent = kategoriFilter;
-     retter.forEach(ret => {
-         console.log(ret.kategori, kategoriFilter);
-         if (ret.kategori == kategoriFilter || kategoriFilter == "alle") {
-
-             console.log(ret.kategori, kategoriFilter);
-             let klon = temp.cloneNode(true).content;
-             klon.querySelector(".data-navn").textContent = ret.navn;
-             klon.querySelector(".data-billede").src = "imgs/" + ret.billede + ".jpg";
-             klon.querySelector(".data-billede").addEventListener("click", () => {
-                 visModal(ret);
-             });
-             klon.querySelector(".data-billede").alt = "billede" + ret.navn;
-             klon.querySelector(".data-billede").addEventListener("click", () => {
-                 window.location.href = "single.html?id=" + ret.id;
-             });
-
-
-             modtager.appendChild(klon);
-         }
-     });
- }
+    kategoriFilter = this.getAttribute("data-kategori");
+    //console.log(kategoriFilter);
+    visRetter();
 
 
 
+}
 
- // MODAL VINDUE
- // function visModal(retter) {
- //     let modal = document.querySelector("#modal");
- //     modal.classList.add("vis");
- //     modal.querySelector(".modal-navn").textContent = retter.navn;
- //     modal.querySelector(".modal-billede").src = "imgs/" + retter.billede + ".jpg";
- //     modal.querySelector(".modal-billede").alt = "Foto af" + retter.navn;
- //     modal.querySelector(".modal-langbeskrivelse").textContent = retter.langbeskrivelse;
- //     modal.querySelector("button").addEventListener("click", skjulModal);
- //
- // }
- //
- // function skjulModal() {
- //     //window.scrollTo(0);
- //     modal.classList.remove("vis");
- //
- // }
+//viser alle retter i array ved at klone nedestående data, eventlisener på billederne således at single.html kaldes hvis der klikkes på billedet
 
+function visRetter() {
+    let temp = document.querySelector(".retter-template");
+    let modtager = document.querySelector(".main-container");
+    modtager.textContent = "";
+    document.querySelector("header h3").textContent = kategoriFilter;
+    retter.forEach(ret => {
+        console.log(ret.kategori, kategoriFilter);
+        if (ret.kategori == kategoriFilter || kategoriFilter == "alle") {
 
- // STICKY NAV
- window.onscroll = function () {
-     myFunction()
- };
-
-
- let navbar = document.getElementById("navbar");
+            console.log(ret.kategori, kategoriFilter);
+            let klon = temp.cloneNode(true).content;
+            klon.querySelector(".data-navn").textContent = ret.navn;
+            klon.querySelector(".data-billede").src = "imgs/" + ret.billede + ".jpg";
+            klon.querySelector(".data-billede").addEventListener("click", () => {
+                visModal(ret);
+            });
+            klon.querySelector(".data-billede").alt = "billede" + ret.navn;
+            klon.querySelector(".data-billede").addEventListener("click", () => {
+                window.location.href = "single.html?id=" + ret.id;
+            });
 
 
- let sticky = navbar.offsetTop;
+            modtager.appendChild(klon);
+        }
+    });
+}
 
- function myFunction() {
-     if (window.pageYOffset >= sticky) {
-         navbar.classList.add("sticky")
-     } else {
-         navbar.classList.remove("sticky");
-     }
- }
+
+
+
+// MODAL VINDUE
+// function visModal(retter) {
+//     let modal = document.querySelector("#modal");
+//     modal.classList.add("vis");
+//     modal.querySelector(".modal-navn").textContent = retter.navn;
+//     modal.querySelector(".modal-billede").src = "imgs/" + retter.billede + ".jpg";
+//     modal.querySelector(".modal-billede").alt = "Foto af" + retter.navn;
+//     modal.querySelector(".modal-langbeskrivelse").textContent = retter.langbeskrivelse;
+//     modal.querySelector("button").addEventListener("click", skjulModal);
+//
+// }
+//
+// function skjulModal() {
+//     //window.scrollTo(0);
+//     modal.classList.remove("vis");
+//
+// }
+
+
+// STICKY NAV - gør at navbar ikke rykker sig når brugeren scroller ned langs siden
+window.onscroll = function () {
+    StickyNav()
+};
+
+
+let navbar = document.getElementById("navbar");
+
+
+let sticky = navbar.offsetTop;
+
+function StickyNav() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+};
